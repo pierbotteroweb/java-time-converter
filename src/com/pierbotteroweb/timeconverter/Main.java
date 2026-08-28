81ebd196d3f3839726952ea6d117f905b45a2e8c
Pier Bottero <pierbotteroweb@gmail.com>
2026-08-28 18:40:40 -0300

    Correcoes 2

--- src/com/pierbotteroweb/timeconverter/Main.java
+++ src/com/pierbotteroweb/timeconverter/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
         TimeConverter converter = new TimeConverter(3661);
 
-        int minutes = converter.convertToMinutes()
+        int minutes = converter.convertToMinutes();
 
         System.out.println("Tempo em minutos: "+minutes);
 
