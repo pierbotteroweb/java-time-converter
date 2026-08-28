b162a7ac503920df8c7c29e090ee6439143adf03
Pier Bottero <pierbotteroweb@gmail.com>
2026-08-28 18:33:03 -0300

    Correcoes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args){
         TimeConverter converter = new TimeConverter(3661);
 
-        System.out.println("Tempo em minutos: "+converter.convertToMinutes());
+        int minutes = converter.convertToMinutes()
+
+        System.out.println("Tempo em minutos: "+minutes);
 
     }
 
--- src/TimeConverter.java
+++ src/TimeConverter.java
@@ -8,5 +8,5 @@ public class TimeConverter {
 
     public int convertToMinutes(){
         return totalSeconds/60;
-    };
+    }
 }
